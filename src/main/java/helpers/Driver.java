6c2b6354f3717503476c5e9c6ded621efd1f06b5
Andrew Godunov <an.godunov@yandex.ru>
2023-09-23 17:27:22 +0400

    FEAT: project initialization

---
+++ src/main/java/helpers/Driver.java
@@ -0,0 +1,38 @@
+package helpers;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class Driver {
+
+    @BeforeEach
+    public void setupDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-infobars",
+                "--incognito",
+                "--disable-translate",
+                "--disable-web-security");
+        Configuration.browserCapabilities = chromeOptions;
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
+    }
+
+    @AfterEach
+    public void closeDriver() {
+        Selenide.closeWebDriver();
+    }
+
+    public void open(String url) {
+        Selenide.open(url);
+    }
+
+    public void refresh() {
+        Selenide.refresh();
+    }
+}
